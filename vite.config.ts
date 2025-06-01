e67109059f2c26bffb852645bf63110ec52922d2
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-05-31 18:19:57 -0700

    nope, put that back

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "https://timinc-cobble-help.github.io/cobblemon-label-assigner/",
 });
